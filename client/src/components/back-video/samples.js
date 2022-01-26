a25780f49fb2dd434a0b07c4bc61f16ac5b114fc
Abonty18 <labibafarah2998@gmail.com>
2022-01-26 21:44:36 +0600

    added video

--- client/src/components/back-video/samples.js
+++ client/src/components/back-video/samples.js
@@ -1,18 +1,18 @@
 const backVideo = [
   {
     id: 1,
-    name: 'Introduction to express JS',
-    webPreviewUrl: 'https://www.youtube.com/watch?v=L72fhGm1tfE',
+    name: 'Happy Gardening',
+    webPreviewUrl: 'https://www.youtube.com/watch?v=abpiG8ppxCU&list=PLvE-WOB38X7Z8mAiNYyqcldMQQZCnz5o7',
   },
   {
     id: 2,
-    name: 'Introduction to django',
-    webPreviewUrl: 'https://www.youtube.com/watch?v=K74_bKyNDd4',
+    name: 'Enjoy your journey',
+    webPreviewUrl: 'https://www.youtube.com/watch?v=k05M0HQ5pPU&list=PLUSbvZMbeWFpCGPjAvWOExfpL9Cw3vFG0',
   },
   {
     id: 3,
-    name: 'Introduction to laravel',
-    webPreviewUrl: 'https://www.youtube.com/watch?v=EU7PRmCpx-0',
+    name: 'Arts and crafts',
+    webPreviewUrl: 'https://www.youtube.com/watch?v=jnTL4c9fgeM&list=PLkc5kS6oh1_nv5ziCiR_wPcrNOy9r4VsE',
   },
 ];
 
--- client/src/components/dashboard/index.jsx
+++ client/src/components/dashboard/index.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 import Sidebar from '../common/Sidebar';
 import Card from '../common/Card';
 import samples from './samples';
+import { useState } from "react";
+import { Redirect,useParams, useHistory, Link } from 'react-router-dom';
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
-const Dashboard = () => (
+const Dashboard = () => {
+  const [error, setError] = useState(false);
+  const history = useHistory();
+  const user = JSON.parse(localStorage.getItem('profile'));
+  if (!user) {
+    return <Redirect to="/auth" />
+  }
+  const handleSubmit = () => {
+    if (!user ) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+     
+      history.push("/Dashboard");
+    }
+
+   
+  };
+  return(
   <div className="flex flex-grow 1">
     <Sidebar />
     <div className="bg-white-400 w-4/5">
@@ -12,6 +34,7 @@ const Dashboard = () => (
       </div>
     </div>
   </div>
-);
+  );
+};
 
 export default Dashboard;
